64945192293a4d1c43b1ea442c9cf81ac30d14c7
eric ouano <145930414+eriquano@users.noreply.github.com>
2024-07-01 17:37:25 -0500

    Update linesearch.py

--- linesearch.py
+++ linesearch.py
@@ -2,51 +2,6 @@ import jax
 import jax.numpy as jnp
 from jax import grad
 
-# #1D function to minimize
-# def f(x,alpha,p):
-#     #p is the descent direction, alpha is the step length, x is an x value along descent
-
-
-#     return 
-
-# #univariate function 
-# def phi(alpha):
-#     return f(x,alpha,p)
-
-# #sufficient decrease condition:
-# '''phi(alpha_k) <= phi(0) +c*alpha_k*phi'(0)'''
-
-# #if given guess alpha_0, if we have phi(alpha_0) <= phi(0) +c*alpha_k*phi'(0), condition is satisfied and we end search
-# #if not, we know [0, alpha_0] contains acceptable step length
-
-
-# #quadratic approx phi_q(alpha) of phi(alpha):
-# #KNOWN: phi(0), phi'(0), phi(alpha_0)
-# def phi_q(alpha):
-#     diff = (phi(alpha_0) - phi(0) - alpha_0 * grad(phi)(0)) / alpha_0**2
-#     return diff * alpha**2 + grad(phi)(0) * alpha + phi(0)
-
-# #test new trial alpha: alpha_1, defined as minimizer of phi_q
-
-# alpha_1 = -(grad(phi)(0) * alpha_0**2) / (2 * (phi(alpha_0) - phi(0) - grad(phi)(0) * alpha_0)) 
-
-# #if this meets SDC, terminate search
-# #otherwise create cubic function for interpolation again
-# abdot = jnp.dot(jnp.array([[alpha_0**2, -alpha_1**2],[-alpha_0**3, alpha_1**3]]), jnp.array([[phi(alpha_1) - phi(0) - grad(phi)(0) * alpha_1], [phi(alpha_0) - phi(0) - grad(phi)(0) * alpha_0]]))
-# [a, b] = (1 / (alpha_0**2 * alpha_1**2 * (alpha_1 - alpha_0))) * abdot
-
-# def phi_c(alpha):
-#     return a * alpha**3 + b * alpha**2 + alpha * grad(phi)(0) + phi(0) #might not be necessary to compute
-
-# #differentiating phi_c reveals that alpha_2 minimizer of phi_c is between [0,alpha_1] and is given by:
-# alpha_2 = (-b + jnp.sqrt(b**2 - 3 * a * grad(phi)(0))) / (3*a)
-
-# #this process is repeated if needed using cubic interpolant of phi(0), phi'(0), and 2 most recent phi until satisfying alpha is located
-
-# '''if any alpha_i is too close to the previous or too much smaller than previous, we reset alpha_i = alpha_i-1 / 2'''
-
-
-# #OR CUBIC INTERPOLATION THE WHOLE TIME
 
 #-----------------------------------------------------------#
 
